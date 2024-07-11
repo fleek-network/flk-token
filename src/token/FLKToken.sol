5946d933a34aa4195216623c49335a6792a52c1b
N <n@push0.dev>
2024-07-11 11:43:44 -0400

    solmate token

--- src/token/FLKToken.sol
+++ src/token/FLKToken.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @notice The FLK token
-contract FLKToken is ERC20Permit, Ownable {
+contract FLKToken is ERC20, Ownable {
     error InvalidSender();
 
     mapping(address => bool) isMinter;
@@ -17,12 +16,12 @@ contract FLKToken is ERC20Permit, Ownable {
     }
 
     constructor(
-        address owner,
+        address _owner,
         string memory _name,
         string memory _symbol,
         uint256 startingSupply
-    ) ERC20Permit(_name) ERC20(_name, _symbol) Ownable(owner) {
-        _mint(owner, startingSupply);
+    ) ERC20(_name, _symbol, 18) Ownable(_owner) {
+        _mint(_owner, startingSupply);
     }
 
     function mint(address to, uint256 amount) external onlyMinter {
