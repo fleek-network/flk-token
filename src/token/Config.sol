182b9e7fbef4cd95e6437b80afd7614bf58815b3
daltoncoder <daltoncoder2@gmail.com>
2024-07-30 11:56:22 -0400

    update multisig address

--- src/token/Config.sol
+++ src/token/Config.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-address constant OWNER = address(0xF75F494fDd92E59e5b3289998cCcc3b8a284dF70);
+address constant OWNER = address(0x91cF969cd005D04F625F50A33DcD7E8D4fe39f02);
 
 uint256 constant STARTING_SUPPLY = 100000000000000000000000000;
 
