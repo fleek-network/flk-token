e870a842772e1b30721f20a23c605f96686ef05d
daltoncoder <daltoncoder2@gmail.com>
2024-07-30 10:52:29 -0400

    Update config

--- src/token/Config.sol
+++ src/token/Config.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-// todo
-address constant OWNER = address(0);
-// todo
-uint256 constant STARTING_SUPPLY = 1000000;
-// todo
-string constant NAME = "Fleek Network Token";
-// todo
+address constant OWNER = address(0xF75F494fDd92E59e5b3289998cCcc3b8a284dF70);
+
+uint256 constant STARTING_SUPPLY = 100000000000000000000000000;
+
+string constant NAME = "Fleek";
+
 string constant SYMBOL = "FLK";
